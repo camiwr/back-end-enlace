8716a7e0cd10888a044328f0c6c9b1492059ba5a
Camilla Soares Sousa <camillasoares818@gmail.com>
2025-07-24 01:02:47 -0300

    feat: atualiza validação de e-mail no registro e traduz mensagens de erro

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,13 +10,13 @@ export class AuthService {
     constructor(private prisma: PrismaService, private jwtService: JwtService) {}
     async register(name: string, email: string, password: string) {
   // Verifica se o e-mail pertence ao domínio do IFPI
-  if (!email.endsWith('@ifpi.edu.br')) {
-    throw new BadRequestException('Email must be from IFPI domain');
+  if (!email.endsWith('@aluno.ifpi.edu.br')) {
+    throw new BadRequestException('O e-mail deve ser do domínio IFPI');
   }
 
   // Verifica se o e-mail já existe no banco de dados
   const existingUser = await this.prisma.user.findUnique({ where: { email } });
-  if (existingUser) throw new BadRequestException('Email already in use');
+  if (existingUser) throw new BadRequestException('Email já cadastrado');
 
   // Criptografa a senha
   const hashedPassword = await bcrypt.hash(password, 10);
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Enlace API')
+    .setDescription('Documentação da API da plataforma Enlace (Comunidades e Eventos acadêmicos)')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = parseInt(process.env.PORT ?? '3000');
+
+  await app.listen(port);
+  console.log(`🚀 Server rodando em http://localhost:${port}`);
+  console.log(`📚 Docs disponíveis em http://localhost:${port}/api`);
+
 }
 bootstrap();
